0636f0b1a08cf0861d6473c1e9aa2409bf83f3cc
최세원 <choisewon@choesewon-ui-MacBook-Air.local>
2021-06-07 15:02:49 +0900

    Updates

---
+++ precache-manifest.9094d7435ddeb0cc5796b50be58fb737.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037b8ff8a45da10dc35832833eb3bbb2",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "f88cbb5d15f60debc3dc",
+    "url": "/developerFolio/static/css/3.d29f9f24.chunk.css"
+  },
+  {
+    "revision": "abc8034159c206ed4a66",
+    "url": "/developerFolio/static/css/4.7369bb08.chunk.css"
+  },
+  {
+    "revision": "46d3ceb887c020c757c4",
+    "url": "/developerFolio/static/css/main.99f11e2b.chunk.css"
+  },
+  {
+    "revision": "f4157c30bcde03401b95",
+    "url": "/developerFolio/static/js/2.d473e79d.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.d473e79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88cbb5d15f60debc3dc",
+    "url": "/developerFolio/static/js/3.38a75f35.chunk.js"
+  },
+  {
+    "revision": "abc8034159c206ed4a66",
+    "url": "/developerFolio/static/js/4.b1d3994d.chunk.js"
+  },
+  {
+    "revision": "46d3ceb887c020c757c4",
+    "url": "/developerFolio/static/js/main.24d2360c.chunk.js"
+  },
+  {
+    "revision": "042fb52a0e6f38a659d0",
+    "url": "/developerFolio/static/js/runtime-main.ce1f5bcf.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/developerFolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/developerFolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "d5b7221159e92aadcd50d6d3a2ee8ea2",
+    "url": "/developerFolio/static/media/bangaloreUniLogo.d5b72211.png"
+  },
+  {
+    "revision": "0e2e9b8f5b7a304828125e9d863d331e",
+    "url": "/developerFolio/static/media/conservativeLogo.0e2e9b8f.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "555dc6322daeb59cc38f388e38c49675",
+    "url": "/developerFolio/static/media/kccaLogo.555dc632.png"
+  },
+  {
+    "revision": "d9df0fde76adbfb9a285767e3d956c1e",
+    "url": "/developerFolio/static/media/login.d9df0fde.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "6628bb9e43ba2eca3204b8a055839c06",
+    "url": "/developerFolio/static/media/myWitter.6628bb9e.png"
+  },
+  {
+    "revision": "b8af859fa874d5b3ac79c9e226d56a88",
+    "url": "/developerFolio/static/media/poscoLogo.b8af859f.png"
+  },
+  {
+    "revision": "2a9ddd5216b3843a787d333b2273e253",
+    "url": "/developerFolio/static/media/senecaLogo.2a9ddd52.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "f60dd288f9e3dbe44bfff6c28d742544",
+    "url": "/developerFolio/static/media/winter.f60dd288.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3d7621faf74d813d479c177f11b442a.js"
+  "/developerFolio/precache-manifest.9094d7435ddeb0cc5796b50be58fb737.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
